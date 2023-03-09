1c62d07826dcc17ae225b266edb65544ba2d5b9d
*Anna <roxy.sarov@mail.ru>
2023-03-09 18:02:22 +0300

    ДЗ задачи 2, 3, файл README

--- Homework/Homework_03/Ex03/Program.cs
+++ Homework/Homework_03/Ex03/Program.cs
@@ -2,3 +2,15 @@
 //3 -> 1, 8, 27
 //5 -> 1, 8, 27, 64, 125
 
+Console.Write("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+
+Coub(N);
+
+void Coub(int N)
+{
+for(int i = 1; i <= N; i++)
+{
+    Console.WriteLine(i*i*i);
+}
+}
