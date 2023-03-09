489d34c4e5472e92f4be16ab3667bd07e4d50229
*Anna <roxy.sarov@mail.ru>
2023-03-09 22:31:03 +0300

    Семинар 4

---
+++ Seminar/Seminar_04/Ex01/Program.cs
@@ -0,0 +1,20 @@
+﻿//Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+//7 -> 28
+//4 -> 10
+//8 -> 36
+
+Console.Write("Введите число ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Sum(A);
+
+void Sum(int A)
+{
+int S = 0;
+for(int i = 0; i <= A; i++)
+{
+    S = S + i;
+}
+Console.Write(S);
+}
+
