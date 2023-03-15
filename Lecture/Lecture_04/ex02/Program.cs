f065f045b08c1d7449dfdd0f8a07da1522b955f6
*Anna <roxy.sarov@mail.ru>
2023-03-15 17:46:25 +0300

    Лекция 4

---
+++ Lecture/Lecture_04/ex02/Program.cs
@@ -0,0 +1,36 @@
+﻿//Алгоритм, позволяющий закрашивать замкнутые области.
+
+int[,] pic = new int[,]; //массив с картинкой
+
+//Метод, который выводит картинку в двумерный массив.
+void PrintImage(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < image.GetLength(1); j++)
+        {
+            //Console.Write($"{image[i, j]} ");
+            if (image[i, j] == 0) Console.WriteLine($" ");
+            else Console.WriteLine($"+");
+        }
+        Console.WriteLine();
+    }
+}
+
+PrintImage(pic);
+
+//Метод, который закрашивает картинку.
+void FillImage(int row, int col)
+{
+    if (pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row - 1, col);
+        FillImage(row, col - 1);
+        FillImage(row + 1, col);
+        FillImage(row, col + 1);
+    }
+}
+
+FillImage(13, 13);
+PrintImage(pic);
---
+++ Lecture/Lecture_04/ex03/Program.cs
@@ -0,0 +1,15 @@
+﻿//Метод, который находит простой факториал.
+double Factorial(int n) //Если заменить int на double, значения будут отображаться корректно.
+
+{
+    // 1! = 1
+    // 0! = 1
+    if (n == 1) return 1;
+    else return n * Factorial(n - 1);
+}
+Console.WriteLine(Factorial(3)); //3! = 1*2*3 = 6
+
+for (int i = 1; i < 40; i++)
+{
+    Console.WriteLine($"{i}! = {Factorial(i)}");
+}
