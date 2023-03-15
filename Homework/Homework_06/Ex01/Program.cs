4312fec9e629c6fb2d89c27f4f4e57cd80e99998
*Anna <roxy.sarov@mail.ru>
2023-03-15 22:27:08 +0300

    ДЗ 6

--- Homework/Homework_06/Ex01/Program.cs
+++ Homework/Homework_06/Ex01/Program.cs
@@ -2,3 +2,24 @@
 //0, 7, 8, -2, -2 -> 2
 //1, -7, 567, 89, 223-> 3
 
+Console.Write("Введите количество чисел ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+
+Check(NumberA);
+
+void Check(int NumberA)
+{
+    int Var = 0;
+    int Count = 0;
+    for (int i = 0; i < NumberA; i++)
+    {
+        Console.Write("Введите число ");
+        Var = Convert.ToInt32(Console.ReadLine());
+        if (Var > 0)
+        {
+            Count++;
+        }
+    }
+    Console.WriteLine();
+    Console.Write($"Количество чисел больше 0: {Count}.");
+}
