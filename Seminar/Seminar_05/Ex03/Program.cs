0fb0306e81bf806cbc5eadc49c2392971cf4832d
*Anna <roxy.sarov@mail.ru>
2023-03-15 19:02:49 +0300

    Структура

--- Seminar/Seminar_05/Ex03/Program.cs
+++ Seminar/Seminar_05/Ex03/Program.cs
@@ -41,3 +41,24 @@ FillArray(array);
 Console.WriteLine();
 int Count = Number("Введите число: ");
 Check(array, Count);
+
+//или
+
+//bool Check(int[] array, int el)
+//{
+//    int size = array.Length;
+//    for (int i = 0; i < size; i++)
+//        if (array[i] == el) return true;
+//    return false;
+//}
+
+//string PrintResult(int[] array, int el, bool exist)
+//{
+//  if (exist) return $"В массиве [{String.Join(' ', array)}] элемент {el} присутствует";
+//    return $"В массиве элементов нет";
+//}
+
+//int[] arr = { 1, 2, 3, 4, 5, 6 };
+//int find = 1;
+//bool c = Check(arr, find);
+//Console.WriteLine(PrintResult(arr, find, c));
