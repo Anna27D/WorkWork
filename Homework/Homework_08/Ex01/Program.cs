1d7d45fb8a7b3eb5685534e8ecfa8f025205461e
*Anna <roxy.sarov@mail.ru>
2023-03-24 18:37:28 +0300

    ДЗ8

--- Homework/Homework_08/Ex01/Program.cs
+++ Homework/Homework_08/Ex01/Program.cs
@@ -9,3 +9,70 @@
 // 9 5 3 2
 // 8 4 4 2
 
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    string ReadInput = Console.ReadLine();
+    int result = int.Parse(ReadInput);
+    return result;
+}
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void MakeInOrder(int[,] matrix)
+{
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            for (int g = j + 1; g < matrix.GetLength(1); g++)
+            {
+                int temp = 0;
+                if (matrix[i, j] < matrix[i, g])
+                {
+                    temp = matrix[i, g];
+                    matrix[i, g] = matrix[i, j];
+                    matrix[i, j] = temp;
+                }
+            }
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int m = GetNumber("Введите количество строк в массиве: ");
+int n = GetNumber("Введите количество столбцов в массиве: ");
+Console.WriteLine();
+
+int[,] matrix = new int[m, n];
+
+FillArray(matrix);
+Console.WriteLine("Начальный массив: ");
+PrintArray(matrix);
+Console.WriteLine();
+Console.WriteLine("Отсортированный массив: ");
+MakeInOrder(matrix);
--- Homework/Homework_08/Ex02/Program.cs
+++ Homework/Homework_08/Ex02/Program.cs
@@ -1,4 +1,5 @@
-﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+﻿// Задайте прямоугольный двумерный массив.
+// Напишите программу, которая будет находить строку с наименьшей суммой элементов.
 
 // Например, задан массив:
 // 1 4 7 2
@@ -7,3 +8,73 @@
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка.
 
+int GetNumber(string message)
+{
+    Console.Write(message);
+    string ReadInput = Console.ReadLine();
+    int result = int.Parse(ReadInput);
+    return result;
+}
+
+void FillArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = new Random().Next(0, 10);
+        }
+    }
+}
+
+void PrintArray(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+void Sum(int[,] matrix)
+{
+    int Sum = 0;
+    int SumIndex = 0;
+    int min;
+    for (int j = 0; j < matrix.GetLength(0); j++)
+    {
+        Sum = Sum + matrix[0, j];
+    }
+    min = Sum;
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Sum = 0;
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Sum = Sum + matrix[i, j];
+        }
+        Console.WriteLine($"Сумма {i} строки: {Sum}.");
+        if (Sum < min)
+        {
+            SumIndex = i;
+            min = Sum;
+        }
+    }
+    Console.WriteLine();
+    Console.WriteLine($"Номер строки с наименьшей суммой элементов: {SumIndex}.");
+}
+
+int m = GetNumber("Введите количество строк и столбцов в массиве: ");
+int n = m;
+Console.WriteLine();
+
+int[,] matrix = new int[m, n];
+
+FillArray(matrix);
+Console.WriteLine("Массив: ");
+PrintArray(matrix);
+Console.WriteLine();
+Sum(matrix);
